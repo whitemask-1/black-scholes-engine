aaff9a2f04cbd9ff0262561b42ba64d250bbd5e3
whitemask-1 <whitemask.exe.1@gmail.com>
2026-03-19 13:44:42 -0400

    dev/wrote greek computation functions

---
+++ bsm_engine/lib/greeks.ml
@@ -0,0 +1,27 @@
+let delta s k t r sigma option_type =
+    let (d1, _) = Core.compute_d1_d2 s k t r sigma in
+    if option_type = "call" then Core.norm_cdf d1
+    else Core.norm_cdf d1 -. 1.0
+
+let gamma s k t r sigma =
+    let (d1, _) = Core.compute_d1_d2 s k t r sigma in
+    Core.norm_pdf d1 /. (s *. sigma *. sqrt t)
+
+let vega s k t r sigma =
+    let (d1, _) = Core.compute_d1_d2 s k t r sigma in
+    S *. Core.norm_pdf d1 *. sqrt t
+
+let theta s k t r sigma option_type =
+    let (d1, d2) = Core.compute_d1_d2 s k t r sigma in
+    let term1 = -.(s *. Core.norm_pdf(d1) *. sigma) /. (2.0 *. sqrt t) in
+    if option_type = "call" then abs_float (term1 -. r *. k *. exp (-. r *. t) *. Core.norm_cdf d2)
+    else abs_float (term1 +. r *. k *. exp (-. r *. t) *. Core.norm_cdf (-. d2))
+
+let theta_daily s k t r sigma =
+  theta s k t r sigma option_type /. 365.0
+
+let rho s k t r sigma option_type =
+    let (_, d2) = Core.compute_d1_d2 s k t r sigma in
+    if option_type = "call" then k *. t *. exp (-. r *. t) *. Core.norm_cdf d2
+    else -. k *. t *. exp (-. r *. t) *. Core.norm_cdf (-. d2)
+
