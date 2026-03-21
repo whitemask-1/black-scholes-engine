57f4959623c92c21447c4af773391d552bb17452
whitemask-1 <whitemask.exe.1@gmail.com>
2026-03-20 20:24:58 -0400

    dev/created new display live chain function
    now shows the expiration date, whether its a call/put chain, the current
    stock price

--- rich_out.py
+++ rich_out.py
@@ -1,11 +1,22 @@
 from rich.table import Table
 from rich.console import Console
+from market import find_ticker_stock_price
 
-
-def display_chain(df, symbol):
+def display_manual_chain(df, symbol="Manual"):
     console = Console()
 
     table = Table(title=f" {symbol} Options Chain")
+    for col in df.columns:
+        table.add_column(col, justify="right")
+    for _, row in df.iterrows():
+        table.add_row(*[f"{v:.4f}" if isinstance(v, float) else str(v) for v in row])
+    console.print(table)
+
+def display_live_chain(df, symbol, expiration_date, option_type):
+    console = Console()
+
+    current_stock_price = find_ticker_stock_price(symbol)
+    table = Table(title=f" {symbol} {option_type.upper()} Options Chain - {expiration_date} - (Current Stock Price = {current_stock_price})")
     for col in df.columns:
         table.add_column(col, justify="right")
     for _, row in df.iterrows():
